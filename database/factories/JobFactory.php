dce2eb5e2cb00ce2e27002c5424b4bf28d01bc93
SaidHafizzan <100667330+SaidInamul@users.noreply.github.com>
2024-11-17 22:03:49 +0800

    Model Factories
    creating factory file and run it in php artisan tinker. Also, trying to generate few record with passing argument 10 in factory method.

---
+++ database/factories/JobFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Job;
+use App\Models\Employer;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Job>
+ */
+class JobFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+
+    protected $model = Job::class; 
+    
+    public function definition(): array
+    {
+        return [
+            'title' => fake()->jobTitle(),
+            'salary' => fake()->numberBetween(25000, 50000),
+            'employer_id' => Employer::factory(),
+        ];
+    }
+}
