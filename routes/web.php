3a619516fbf4d4d7bca9a9bc4331d26149499007
SaidHafizzan <100667330+SaidInamul@users.noreply.github.com>
2024-11-19 21:18:37 +0800

    Always Validate. Never Trust the User.
    Adding the validation rule when user click the submit button.

--- routes/web.php
+++ routes/web.php
@@ -27,10 +27,16 @@ Route::get('/jobs/{id}', function ($id) {
 });
 
 Route::post('/jobs', function (){
+
+    request()->validate([
+        'title' => ['required', 'min:3'],
+        'salary' => ['required', 'integer'],
+    ]);
+
     Job::create([
         'title' => request('title'),
         'salary' => request('salary'),
-        'employer_id' => 1, 
+        'employer_id' => 2, 
     ]);
 
     return redirect('/jobs');
